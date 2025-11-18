2a56c56ed951d644a8f207d2302a5379d55bed4c
Filipe Silva <lipe.pesil@gmail.com>
2025-11-18 01:29:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8637813943-78b84",
+  "appId": "1:201063059582:web:978182b354cfcad4d1882f",
+  "apiKey": "AIzaSyDjSny8QuwWVMuOnVwNfYVdg5C3HcZJXnc",
+  "authDomain": "studio-8637813943-78b84.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "201063059582"
+};
