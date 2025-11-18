487dab6e77e0699abcb22f4a665390bfd660da2e
Filipe Silva <lipe.pesil@gmail.com>
2025-11-18 04:10:21 +0000

    por favor, alinhe esses campos (_for element div_)

--- src/app/(app)/clients/[clientId]/page.tsx
+++ src/app/(app)/clients/[clientId]/page.tsx
@@ -569,7 +569,7 @@ export default function ClientDetailsPage({
               )}
             />
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 items-end">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
               {watchedPoolType === 'quadrilateral' && (
                 <>
                   <FormField
