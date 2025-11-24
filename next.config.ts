c0f078aa0ef2567586029e821f3fa76cf10898c9
Filipe Silva <lipe.pesil@gmail.com>
2025-11-24 23:47:48 +0000

    Estou enfrentando um erro crítico na funcionalidade de "Sincronização co

--- next.config.ts
+++ next.config.ts
@@ -30,11 +30,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
-  }
 };
 
 export default nextConfig;
